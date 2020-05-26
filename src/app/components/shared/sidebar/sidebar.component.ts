48fcdbb42d7d6354ac8a85e2cdae832babae6bdc
Carlos Morales <carlosjosuemn98@gmail.com>
2020-05-26 15:59:55 -0500

    se actualizo sidebar

--- src/app/components/shared/sidebar/sidebar.component.ts
+++ src/app/components/shared/sidebar/sidebar.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit, AfterViewInit, AfterViewChecked, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterViewChecked, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { SidebarService } from '@services/sidebar/sidebar.service';
 import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-sidebar',
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent implements OnInit, AfterViewInit, AfterViewChecked {
+export class SidebarComponent implements OnInit, AfterViewInit, AfterViewChecked, OnDestroy {
     @ViewChild('menuTransportare', { static: true }) menuTransportare: ElementRef;
     menu: any;
     ruta: string;
+    suscriber: Subscription;
     constructor(private sidebarService: SidebarService, private router: Router) {
         this.initData();
         this.ruta = '';
-        this.router.events.subscribe((response: NavigationEnd) => {
+        console.log('antes');
+        this.suscriber = this.router.events.subscribe((response: NavigationEnd) => {
+            console.log('en el suscribe');
+
             if (response.url) {
                 const arrayRuta = response.url.split('/');
                 this.ruta = arrayRuta[2];
-                // console.log(arrayRuta);
             }
         });
     }
@@ -26,6 +30,12 @@ export class SidebarComponent implements OnInit, AfterViewInit, AfterViewChecked
     ngOnInit() {
         // const sidebar = document.querySelector('#accordionSidebar');
         // sidebar.querySelectorAll('li').forEach((li) => console.log(li));
+    }
+
+    ngOnDestroy(): void {
+        if (this.suscriber) {
+            this.suscriber.unsubscribe();
+        }
     }
 
     ngAfterViewInit(): void {
