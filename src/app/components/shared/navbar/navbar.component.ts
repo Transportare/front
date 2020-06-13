d395f4ea2a96051c06b08e9443a08fedf4018812
Carlos Morales <carlosjosuemn98@gmail.com>
2020-06-13 12:34:21 -0500

    se agrego sucursles al navbar

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '@services/login/login.service';
 import { Router } from '@angular/router';
+import { SucursalesService } from '@services/utils/sucursales.service';
 
 @Component({
     selector: 'app-navbar',
@@ -8,9 +9,38 @@ import { Router } from '@angular/router';
     styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-    constructor(private loginService: LoginService, private router: Router) {}
+    sucursales: any[];
+    sucursalSelected: any;
+    loading: boolean;
 
-    ngOnInit() {}
+    constructor(private loginService: LoginService, private sucursalesService: SucursalesService, private router: Router) {
+        this.loading = false;
+        this.sucursales = [];
+        this.sucursalSelected = { id: '', text: 'Seleccione Sucursal' };
+    }
+
+    ngOnInit() {
+        this.listarSucursales();
+        this.sucursalesService.changeSucursal().subscribe((response) => {
+            this.sucursalSelected = this.sucursales.find((sucursal) => sucursal.id === response);
+        });
+    }
+
+    listarSucursales() {
+        this.loading = true;
+        this.sucursalesService.getSucursales().subscribe((response: any) => {
+            this.sucursales = response.map((item) => ({ id: item.IdSucursal, text: item.Nombre }));
+            const id = this.sucursalesService.getSucursal();
+            if (id) {
+                this.sucursalSelected = this.sucursales.find((s) => s.id === id);
+            }
+            this.loading = false;
+        });
+    }
+
+    changeSucursal(event) {
+        console.log(event);
+    }
 
     logOut() {
         this.loginService.logOut();
