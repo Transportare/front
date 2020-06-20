6d967d0b6e523b1d93472f567423926747b9ca5e
Carlos Morales <carlosjosuemn98@gmail.com>
2020-06-20 16:49:41 -0500

    change api

--- src/config/api.route.ts
+++ src/config/api.route.ts
@@ -1,2 +1,1 @@
-// export const API_URL = 'http://ec2-18-191-181-65.us-east-2.compute.amazonaws.com/api/';
-export const API_URL = 'http://localhost:3000/api/';
+export const API_URL = 'http://ec2-18-222-220-123.us-east-2.compute.amazonaws.com/api/';
