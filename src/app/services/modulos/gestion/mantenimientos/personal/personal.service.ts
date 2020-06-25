5a6800123cff43b6af51853ae07fa2b7b3d72695
Carlos Morales <carlosjosuemn98@gmail.com>
2020-06-24 23:55:23 -0500

    personal post 50%

--- src/app/services/modulos/gestion/mantenimientos/personal/personal.service.ts
+++ src/app/services/modulos/gestion/mantenimientos/personal/personal.service.ts
@@ -1,14 +1,72 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { API_URL } from 'config/api.route';
+import { Observable } from 'rxjs';
+import { TablaGeneralService } from '@services/utils/tablageneral.service';
+import { Grupo } from '@models/grupo';
+import { map } from 'rxjs/operators';
+import { UbigeoService } from '@services/utils/ubigeo.service';
+import { Ubigeo } from '@models/ubigeo';
 
 @Injectable({
     providedIn: 'root',
 })
 export class PersonalService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private tablaGeneral: TablaGeneralService, private ubigeoService: UbigeoService) {}
 
-    getPersonales() {
-        return this.http.get(`${API_URL}personales`);
+    getPersonales(params) {
+        return this.http.get(`${API_URL}personales`, { params });
+    }
+
+    getSelectsDePersonales(): Observable<{ generos: Grupo[]; estadosCiviles: Grupo[]; tipoPersonales: Grupo[]; departamentos: Ubigeo[] }> {
+        return new Observable((observer) => {
+            this.tablaGeneral
+                .getSelectPorGrupo(1)
+                .pipe(
+                    map((response: Grupo[]) => {
+                        const generos = response;
+                        return generos;
+                    })
+                )
+                .subscribe((generos: Grupo[]) => {
+                    this.tablaGeneral
+                        .getSelectPorGrupo(2)
+                        .pipe(
+                            map((response: Grupo[]) => {
+                                const estadosCiviles = response;
+                                return estadosCiviles;
+                            })
+                        )
+                        .subscribe((estadosCiviles: Grupo[]) => {
+                            this.tablaGeneral
+                                .getSelectPorGrupo(3)
+                                .pipe(
+                                    map((response: Grupo[]) => {
+                                        const tipoPersonales = response;
+                                        return tipoPersonales;
+                                    })
+                                )
+                                .subscribe((tipoPersonales: Grupo[]) => {
+                                    this.ubigeoService
+                                        .getDepartamentos()
+                                        .pipe(
+                                            map((response: Ubigeo[]) => {
+                                                const departamentos = response;
+                                                return departamentos;
+                                            })
+                                        )
+                                        .subscribe((departamentos) => {
+                                            observer.next({
+                                                generos,
+                                                estadosCiviles,
+                                                tipoPersonales,
+                                                departamentos,
+                                            });
+                                            observer.complete();
+                                        });
+                                });
+                        });
+                });
+        });
     }
 }
--- src/app/views/gestion/mantenimientos/personal/formulario/formulario.component.ts
+++ src/app/views/gestion/mantenimientos/personal/formulario/formulario.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { RUTAS_GESTION_MANTENIMIENTOS } from '@routes/rutas-gestion';
+import { PersonalService } from '@services/modulos/gestion/mantenimientos/personal/personal.service';
+import { Grupo } from '@models/grupo';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UbigeoService } from '@services/utils/ubigeo.service';
+import { Ubigeo } from '@models/ubigeo';
 declare var $: any;
 
 @Component({
@@ -9,8 +14,38 @@ declare var $: any;
 })
 export class FormularioComponent implements OnInit, OnDestroy {
     data: any[];
+    generos: Grupo[];
+    generoSelected: Grupo;
+    estadosCiviles: Grupo[];
+    estadoSelected: Grupo;
+    tipoPerfiles: Grupo[];
+    tipoSelected: Grupo;
+    departamentos: Ubigeo[];
+    departamentoSelected: Ubigeo;
+    provincias: Ubigeo[];
+    selectedProvincia: Ubigeo;
+    distritos: Ubigeo[];
+    selectedDistrito: Ubigeo;
+    idPersonal: number;
+    formPersonal: FormGroup;
 
-    constructor(private router: Router) {}
+    constructor(
+        private router: Router,
+        private personalService: PersonalService,
+        private activatedRoute: ActivatedRoute,
+        private fb: FormBuilder,
+        private ubigeoService: UbigeoService
+    ) {
+        this.initData();
+        this.initForm();
+        this.activatedRoute.params.subscribe((params) => {
+            if (params.id) {
+                this.idPersonal = Number(params.id);
+            } else {
+                this.listarSelects();
+            }
+        });
+    }
 
     ngOnInit() {}
 
@@ -18,6 +53,100 @@ export class FormularioComponent implements OnInit, OnDestroy {
         // if (this.msj$) {
         //     this.msj$.unsubscribe();
         // }
+    }
+
+    initData() {
+        this.generos = [];
+        this.estadosCiviles = [];
+        this.tipoPerfiles = [];
+        this.departamentos = [];
+        this.generoSelected = { id: 0, text: 'Seleccione Género', grupo: '' };
+        this.estadoSelected = { id: 0, text: 'Seleccione Estado', grupo: '' };
+        this.tipoSelected = { id: 0, text: 'Seleccione Tipo', grupo: '' };
+        this.departamentoSelected = { id: '', text: 'Seleccione Departamento', padre: '' };
+        this.provincias = [];
+        this.selectedProvincia = { id: '', text: 'Seleccione Provincia', padre: '' };
+        this.distritos = [];
+        this.selectedDistrito = { id: '', text: 'Seleccione Distrito', padre: '' };
+    }
+
+    initForm() {
+        this.formPersonal = this.fb.group({
+            idUbigeo: ['', Validators.required],
+            nombres: ['', Validators.required],
+            apellidos: ['', Validators.required],
+            dni: ['', Validators.required],
+            fecNacimiento: ['', Validators.required],
+            direccion: ['', Validators.required],
+            idGenero: ['', Validators.required],
+            idEstadoCivil: ['', Validators.required],
+            telefono: ['', Validators.required],
+            fecIngreso: ['', Validators.required],
+            idTipoPersonal: ['', Validators.required],
+        });
+    }
+
+    listarSelects() {
+        this.personalService.getSelectsDePersonales().subscribe((response) => {
+            this.generos = response.generos;
+            this.estadosCiviles = response.estadosCiviles;
+            this.tipoPerfiles = response.tipoPersonales;
+            this.departamentos = response.departamentos;
+        });
+    }
+
+    generoChange(event) {
+        this.formPersonal.patchValue({
+            idGenero: event.id,
+        });
+    }
+
+    estadoCivilChange(event) {
+        this.formPersonal.patchValue({
+            idEstadoCivil: event.id,
+        });
+    }
+
+    tipoChange(event) {
+        this.formPersonal.patchValue({
+            idTipoPersonal: event.id,
+        });
+    }
+
+    // Ubigeo
+
+    changeDepartamento(event) {
+        this.selectedProvincia = { id: '', text: 'Seleccione Provincia', padre: '' };
+        this.selectedDistrito = { id: '', text: 'Seleccione Distrito', padre: '' };
+        this.distritos = [];
+        this.getProvincias(event.id);
+    }
+
+    changeProvincia(event) {
+        this.selectedDistrito = { id: '', text: 'Seleccione Distrito', padre: '' };
+        this.getDistritos(event.id);
+    }
+
+    changeDistrito(event) {
+        this.formPersonal.patchValue({
+            idUbigeo: event.id,
+        });
+    }
+
+    getProvincias(id) {
+        this.ubigeoService.getHijos(id).subscribe((response) => {
+            this.provincias = response;
+        });
+    }
+
+    getDistritos(id) {
+        this.ubigeoService.getHijos(id).subscribe((response) => (this.distritos = response));
+    }
+
+    //
+
+    guardarPersonal() {
+        console.log(this.formPersonal.value);
     }
 
     atras() {
