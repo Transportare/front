e4d76c971fcf5486070b40d2d4bbe07d4b552963
Carlos Morales <carlosjosuemn98@gmail.com>
2020-06-27 11:42:51 -0500

    entero positivo directiva creada

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TooltipDirective } from './tooltip.directive';
+import { EnteroPositivoDirective } from './entero-positivo.directive';
 
 @NgModule({
-    declarations: [TooltipDirective],
+    declarations: [TooltipDirective, EnteroPositivoDirective],
     imports: [CommonModule],
-    exports: [TooltipDirective],
+    exports: [TooltipDirective, EnteroPositivoDirective],
 })
 export class DirectivesModule {}
