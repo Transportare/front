5ef2d6cb0e95cb44d80e443a5ca300e2c0df8e53
Carlos Morales <carlosjosuemn98@gmail.com>
2020-07-16 23:21:21 -0500

    se añadio directiva

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TooltipDirective } from './tooltip.directive';
 import { EnteroPositivoDirective } from './entero-positivo.directive';
+import { DropFilesDirective } from './drop-files.directive';
 
 @NgModule({
-    declarations: [TooltipDirective, EnteroPositivoDirective],
+    declarations: [TooltipDirective, EnteroPositivoDirective, DropFilesDirective],
     imports: [CommonModule],
-    exports: [TooltipDirective, EnteroPositivoDirective],
+    exports: [TooltipDirective, EnteroPositivoDirective, DropFilesDirective],
 })
 export class DirectivesModule {}
---
+++ src/app/directives/drop-files.directive.ts
@@ -0,0 +1,76 @@
+import { Directive, EventEmitter, HostListener, Input, Output, OnInit } from '@angular/core';
+import { FileItem } from '@models/index';
+
+@Directive({
+    selector: '[appDropFiles]',
+})
+export class DropFilesDirective implements OnInit {
+    @Input() archivo: FileItem[];
+    @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
+    @Output() errorDrop: EventEmitter<{ message: string; status: boolean }> = new EventEmitter();
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    @HostListener('dragover', ['$event'])
+    public onDragEnter(event: any) {
+        this.prevenirDetener(event);
+        this.mouseSobre.emit(true);
+    }
+
+    @HostListener('dragleave', ['$event'])
+    public onDragLeave(event: any) {
+        this.mouseSobre.emit(false);
+    }
+
+    @HostListener('drop', ['$event'])
+    public onDrop(event: any) {
+        const transferencia = this.getTransferencia(event);
+        if (!transferencia) {
+            return;
+        }
+
+        this.extrarArchivos(transferencia.files);
+        this.prevenirDetener(event);
+        this.mouseSobre.emit(false);
+    }
+
+    private getTransferencia(event: any) {
+        // compatibilidad de navegadores
+        return event.dataTransfer ? event.dataTransfer : event.originalEvent.dataTrasnfer;
+    }
+
+    private extrarArchivos(archivos: FileList) {
+        console.log(archivos);
+        if (archivos.length > 1) {
+            this.errorDrop.emit({ message: '*Solo se acepta un archivo', status: true });
+            return;
+        }
+
+        this.errorDrop.emit({ message: '', status: false });
+
+        if (this.validarTipo(archivos[0])) {
+            this.archivo[0] = new FileItem(archivos[0]);
+        }
+    }
+
+    // Validaciones
+    private prevenirDetener(event: Event) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    private validarTipo(archivo: File): boolean {
+        if (archivo.type === '' || archivo.type === undefined) {
+            return false;
+        }
+
+        if (archivo.name.toLocaleLowerCase().split('.').pop() === 'xlsx' || archivo.name.toLocaleLowerCase().split('.').pop() === 'xls') {
+            this.errorDrop.emit({ message: 'Archivo cargado correctamente.', status: false });
+            return true;
+        } else {
+            this.errorDrop.emit({ message: 'Solo se aceptan archivos con formato excel.', status: true });
+            return false;
+        }
+    }
+}
