71c17cd980c4803fd4284e6779dba4942b45e15e
Carlos Morales <carlosjosuemn98@gmail.com>
2020-08-23 16:41:54 -0500

    cambio de tipado

--- src/app/services/utils/tablageneral.service.ts
+++ src/app/services/utils/tablageneral.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { API_URL } from 'config/api.route';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Grupo } from '@models/index';
 
 @Injectable({ providedIn: 'root' })
 export class TablaGeneralService {
     constructor(private http: HttpClient) {}
 
-    getSelectPorGrupo(id: number) {
+    getSelectPorGrupo(id: number): Observable<Grupo[]> {
         return this.http.get(`${API_URL}tablaGeneral/${id}`).pipe(
             map((response: any) => {
                 return response.data.map((item) => ({
