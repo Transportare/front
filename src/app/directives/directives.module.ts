004d7b0126ee26dd2b8f6b32e8cacc501085c2f2
Carlos Morales <carlosjosuemn98@gmail.com>
2020-08-25 22:15:49 -0500

    directiva y separador de miles añadidos

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { TooltipDirective } from './tooltip.directive';
 import { EnteroPositivoDirective } from './entero-positivo.directive';
 import { DropFilesDirective } from './drop-files.directive';
+import { NumeroDecimalDirective } from './numero-decimal.directive';
 
 @NgModule({
-    declarations: [TooltipDirective, EnteroPositivoDirective, DropFilesDirective],
+    declarations: [TooltipDirective, EnteroPositivoDirective, DropFilesDirective, NumeroDecimalDirective],
     imports: [CommonModule],
-    exports: [TooltipDirective, EnteroPositivoDirective, DropFilesDirective],
+    exports: [TooltipDirective, EnteroPositivoDirective, DropFilesDirective, NumeroDecimalDirective],
 })
 export class DirectivesModule {}
