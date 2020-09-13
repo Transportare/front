266b87df4db4456bd5a6cf1052e1130ca4ee5d18
Carlos Morales <carlosjosuemn98@gmail.com>
2020-09-12 21:51:46 -0500

    se creo login guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
+import { LoginGuard } from '@guards/login.guard';
 import { ViewComponent } from './views/views.component';
 
 const routes: Routes = [
     {
         path: 'login',
+        canActivate: [LoginGuard],
         loadChildren: () => import('./views/login/login.module').then((m) => m.LoginModule),
     },
     {
