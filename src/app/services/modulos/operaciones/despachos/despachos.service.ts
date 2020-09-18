f6af2f03b4ce8c5b9b4869c3cc30b109f035436a
Carlos Morales <carlosjosuemn98@gmail.com>
2020-09-17 23:15:36 -0500

    se boraron clases

--- src/app/services/modulos/operaciones/despachos/despachos.service.ts
+++ src/app/services/modulos/operaciones/despachos/despachos.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { API_URL } from 'config/api.route';
-import { Observable } from 'rxjs';
-import { Ubigeo, Manifiesto, Ruta, PaginacionModel, Grupo } from '@models/index';
 import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
