4b30874178785812456ee8e0f723850f29be6d26
Carlos Josue <carlosjosuemn98@gmail.com>
2022-03-15 23:20:49 -0500

    added message error in failed login

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,55 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from '@services/login/login.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-    formLogin: FormGroup;
-    error: boolean;
+export class LoginComponent implements OnInit, OnDestroy {
+  formLogin: FormGroup;
+  error: boolean;
 
-    constructor(private fb: FormBuilder, private route: Router, private loginService: LoginService) {}
+  private _subscription = new Subscription();
 
-    ngOnInit(): void {
-        this.error = false;
-        this.initForm();
+  constructor(private fb: FormBuilder, private route: Router, private loginService: LoginService) {}
+
+  ngOnInit(): void {
+    this.error = false;
+    this.initForm();
+  }
+
+  ngOnDestroy(): void {
+    this._subscription?.unsubscribe();
+  }
+
+  initForm() {
+    this.formLogin = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required]],
+      error: [false],
+    });
+  }
+
+  get ussernameValid() {
+    console.log('holi');
+    return this.formLogin.get('username').invalid && this.formLogin.get('username').touched;
+  }
+
+  get passwordValid() {
+    return this.formLogin.get('password').invalid && this.formLogin.get('password').touched;
+  }
+
+  get errorControl() {
+    console.log('errpr');
+    return this.formLogin.get('error');
+  }
+
+  ingresar() {
+    if (this.formLogin.invalid) {
+      return;
     }
 
-    initForm() {
-        this.formLogin = this.fb.group({
-            username: ['', Validators.required],
-            password: ['', [Validators.required]],
-        });
-    }
+    const data = {
+      usuario: this.formLogin.value.username,
+      password: this.formLogin.value.password,
+    };
 
-    get ussernameValid() {
-        return this.formLogin.get('username').invalid && this.formLogin.get('username').touched;
-    }
-
-    get passwordValid() {
-        return this.formLogin.get('password').invalid && this.formLogin.get('password').touched;
-    }
-
-    ingresar() {
-        if (this.formLogin.invalid) {
-            return;
-        }
-
-        const data = {
-            usuario: this.formLogin.value.username,
-            password: this.formLogin.value.password,
-        };
-
-        this.loginService.logIng(data).subscribe(
-            (response) => {
-                this.loginService.saveDataUser(response);
-                this.route.navigate(['/dashboard']);
-            },
-            (error) => {}
-        );
-    }
+    this._subscription = this.loginService.logIng(data).subscribe(
+      (response) => {
+        this.loginService.saveDataUser(response);
+        this.errorControl.setValue(false);
+        this.route.navigate(['/dashboard']);
+      },
+      (error) => {
+        this.errorControl.setValue(true);
+      }
+    );
+  }
 }
